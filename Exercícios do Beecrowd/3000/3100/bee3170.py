b248ba0e0b05071c8570ab6646468aca263c0ded
hpalermoemerick <124798752+hpalermoemerick@users.noreply.github.com>
2023-06-05 21:15:24 -0300

    Delete Exercícios do Beecrowd/3000/3100 directory

--- Exercícios do Beecrowd/3000/3100/bee3170.py
+++
@@ -1,7 +0,0 @@
-bolinhas = int(input())
-galhos = int(input()) 
-falta = galhos // 2 - bolinhas
-if falta > 0:
-    print(f"Faltam {falta} bolinha(s)")
-else:
-    print("Amelia tem todas bolinhas!")
